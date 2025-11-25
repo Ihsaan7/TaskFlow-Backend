2578c0d0efd65a0ce574da0b448ea84b3c7aef2e
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-25 21:08:11 +0500

    UserModel Added

---
+++ src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrpt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required!"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required!"],
+      unique: true,
+      lowercase: true,
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required!"],
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required!"],
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hashing pass before saving
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrpt.hash(this.password, 10);
+  next();
+});
+
+// Compairing Hashed password
+userSchema.methods.comparePass = async (givenPass) => {
+  return await bcrpt.compare(givenPass, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
