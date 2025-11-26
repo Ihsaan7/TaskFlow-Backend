18b252679c2dfa00b4eed3b2fd7cd22bbfb65773
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-26 11:24:00 +0500

    Add user routes and middleware integration in app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 import express from "express";
 import AsyncHandler from "./utils/AsyncHandler.js";
 import ApiError from "./utils/ApiError.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
 
-// Test Route for API working
+//----------- USER ROUTES -------------------
+app.use("/api/v1/users", userRouter);
+
+// -----------Test Route ---------------
 app.get("/api/health", (req, res) => {
   res.json({ message: "Api is working fine." });
 });
-
-// Test route for Utils
 app.get(
   "/api/test",
   AsyncHandler(async (req, res) => {
@@ -20,13 +26,13 @@ app.get(
   })
 );
 
-// Centeralized Erorr Handlign
+// ------------Centeralized Erorr Handlign--------------------
 app.use((error, req, res, next) => {
   const statusCode = error.statusCode || 500;
-  const message = error.message || "Internal server Erorr";
+  const message = error.message || "Internal server Error";
 
   res.status(statusCode).json({
-    succes: false,
+    success: false,
     message,
     ...(process.env.NODE_ENV === "development" && { stack: error.stack }),
   });
---
+++ src/routes/user.route.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import verifyToken from "../middelwares/auth.mware.js";
+import { uploadMulter } from "../middelwares/multer.mware.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
+
+const router = Router();
+
+// Register
+router.route("/signup").post(
+  uploadMulter.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+// Login
+router.route("/login").post(loginUser);
+
+export default router;
