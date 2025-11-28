a94e619f1439e6aa495e3555b54e4607e9d094c2
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-28 18:20:47 +0500

    Board model and controller added

---
+++ src/controllers/board.controller.js
@@ -0,0 +1,36 @@
+import board from "../models/board.model.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import AsyncHandler from "../utils/AsyncHandler.js";
+
+const createBoard = AsyncHandler(async (req, res) => {
+  const { title, description, background } = req.body;
+  if ([title, description, background].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields required!");
+  }
+
+  const board = await board.create({
+    title,
+    description,
+    background: background || "#0079BF",
+    owner: req.user?._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, board, "Board created successfully"));
+});
+
+const getAllBoard = AsyncHandler(async (req, res) => {
+  const user = req.user?._id;
+  if (!user) {
+    throw new ApiError(403, "Unauthorized Access or Token expired!");
+  }
+  const boards = await board.find({ owner: user }).sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, boards, "All Boards fetched successfully"));
+});
+
+export { createBoard, getAllBoard };
--- src/models/board.model.js
+++ src/models/board.model.js
@@ -1,37 +1,44 @@
 import mongoose from "mongoose";
 
-const boardSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title required!"],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  isArchived: {
-    type: Boolean,
-    default: false,
-  },
-  members: [
-    {
+const boardSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title required!"],
+      trim: true,
+    },
+    background: {
+      type: String,
+      default: "#0079BF", // Blue like trello
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
-  ],
-  lists: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "List",
+    isArchived: {
+      type: Boolean,
+      default: false,
     },
-  ],
-});
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "List",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const board = mongoose.model("Board", boardSchema);
 export default board;
