84e66c32affb8622aa840939bb0ca3036c865fa1
Ihsaan7 <ihsaan2215@gmail.com>
2025-11-30 23:13:42 +0500

    Refactor card controller for improved readability and maintainability

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -40,6 +40,22 @@ const createCard = AsyncHandler(async (req, res) => {
     .json(new ApiResponse(201, card, "Card created successfully"));
 });
 
+const getCard = AsyncHandler(async (req, res) => {
+  const { cardID } = req.params;
+  if (!cardID) {
+    throw new ApiError(400, "Invalid card ID!");
+  }
+
+  const card = await cardModel.findById(cardID);
+  if (!card) {
+    throw new ApiError(404, "No card found!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, card, "Card fetched successfully"));
+});
+
 const getAllCard = AsyncHandler(async (req, res) => {
   const { listID } = req.params;
   if (!listID) {
@@ -54,6 +70,42 @@ const getAllCard = AsyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, cards, "All cards fetched successfully"));
+});
+
+const updateCard = AsyncHandler(async (req, res) => {
+  // const { title, description, dueDate, members } = req.body;
+  // if ([title, description, dueDate, members].some((field) => !field?.trim())) {
+  //   throw new ApiError(400, "All fields are required!");
+  // }
+
+  // const updatedCard = await cardModel.findByIdAndUpdate(
+  //   cardID,
+  //   {
+  //     title,
+  //     description,
+  //     dueDate,
+  //     members,
+  //   },
+  //   { new: true }
+  // );
+
+  const { cardID } = req.params;
+  if (!cardID) {
+    throw new ApiError(400, "Invalid card ID!");
+  }
+
+  const updateFields = req.body;
+  const updatedCard = await cardModel.findByIdAndUpdate(cardID, updateFields, {
+    new: true,
+  });
+
+  if (!updatedCard) {
+    throw new ApiError(404, "Card not found!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedCard, "Card updated successfully"));
 });
 
 const moveCard = AsyncHandler(async (req, res) => {
@@ -76,4 +128,21 @@ const moveCard = AsyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Card moved successfully"));
 });
 
-export { createCard, getAllCard, moveCard };
+const commentOnCard = AsyncHandler(async (req, res) => {
+  const { cardID } = req.params;
+  if (!cardID) {
+    throw new ApiError(400, "Invalid card ID!");
+  }
+
+  const { text } = req.body;
+  if (!text) {
+    throw new ApiError(400, "Comment is missing!");
+  }
+
+  const user = req.user._id;
+  if (!user) {
+    throw new ApiError();
+  }
+});
+
+export { createCard, getAllCard, moveCard, getCard, updateCard };
