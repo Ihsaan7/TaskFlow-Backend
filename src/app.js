2007581cca37ac775b98a0005d5ea750cfef6181
Ihsaan7 <ihsaan2215@gmail.com>
2025-12-01 20:02:17 +0500

    Refactor CORS configuration to specify allowed origin

--- src/app.js
+++ src/app.js
@@ -22,10 +22,12 @@ const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(cors({ 
-  origin: true,
-  credentials: true 
-}));
+app.use(
+  cors({
+    origin: "https://task-flow-frontend-seven.vercel.app",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/v1/users", userRouter);
